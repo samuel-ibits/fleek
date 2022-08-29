2f96c6a74b142ff3abd6d41ac987f4092c402f1a
samuel-ibits <ibitoyesamuel25@gmail.com>
2022-08-30 00:33:10 +0100

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import numpy as np
+
 
 app = Flask(__name__)
 CORS(app)
